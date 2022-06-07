d40b345cb1c028e5650b57ff11e78c6fa509834f
Miha Stele <stele1000@gmail.com>
2022-06-07 18:04:03 +0200

    Progress

---
+++ blog/posts/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Simple in-memory posts repository
+const posts = {};
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("listening on 4000");
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Purpose of this project
+
+It is just a toy project, meaning it is not production ready, most of stuff is hardcoded, it is not using a DB, ...
+The purpose is to exercise communication between microservices
