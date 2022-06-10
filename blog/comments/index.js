b0d24d24cceaa36720c78e24edaf4cf2a9548dc2
Miha Stele <stele1000@gmail.com>
2022-06-10 21:53:18 +0200

    Emit event impl., add exception handling to avoid crash

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const postId = req.params.id;
   const { content } = req.body;
@@ -24,7 +25,26 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[postId] = comments;
 
-  res.status(201).send(comments);
+  try {
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentCreated",
+      data: {
+        id,
+        content,
+        postId: req.params.id,
+      },
+    });
+
+    res.status(201).send(comments);
+  } catch (err) {
+    res.send();
+  }
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,18 +5,21 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
 
   let port = 4000;
 
   // downside - no error handling if one of the apps is failing, no exception handling techniques
   for (let i = 0; i < 3; i++) {
-    axios.post(`http://localhost:${port}/events`, event);
+    try {
+      await axios.post(`http://localhost:${port}/events`, event);
+    } catch (err) {
+      console.log(err);
+    }
     port++;
-
-    res.send({ status: "ok" });
   }
+  res.send({ status: "ok" });
 });
 
 app.listen(4005, () => {
